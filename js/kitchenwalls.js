a125ee05c1f8d06c548bbd44c9024277956cebcc
ablackwood <andyblackwood@gmail.com>
2014-08-01 11:44:25 +1000

    Stable version of kitchenwalls.js
    Laying out function working.
    Extrude, and Edit Mode still to come.
    Main html code now creates an instance of kitchenwalls.js and calls it's
    functions.

--- js/kitchenwalls.js
+++ js/kitchenwalls.js
@@ -4,26 +4,30 @@ THREE.KitchenWalls = function(){
 	this.plan = [];
 	this.height = 0;
 	this.kitchenWall = new THREE.Object3D();
-	scene.add(this.kitchenWall);
 	this.layingOut = true;
 	this.extruding = false;
 	this.editMode = false;
 	this.complete = false;
+	this.outlineColour = 0x2da4a8;
 
 	this.addPoint = function(x,y){
-		this.plan.push({x: x, y: y});
-		if(this.plan[0].x == this.plan[this.plan.length].x && this.plan[0].y == this.plan[this.plan.length].y){
+		this.plan.push(new THREE.Vector2 (x, y));
+		if(this.plan.length > 1 && this.plan[0].x == this.plan[this.plan.length - 1].x && this.plan[0].y == this.plan[this.plan.length - 1].y){
+			this.plan.pop();
 			this.layingOut = false;
 			this.extruding = true;
+			this.outlineColour = 0xfeaa3a;
 		}
 		this.redraw();
 	};
 
 	this.deletePoint = function(){
+		del = false;
 		this.plan.pop();
 		if(this.plan[0].x != this.plan[this.plan.length].x && this.plan[0].y != this.plan[this.plan.length].y){
 			this.layingOut = true;
 			this.extruding = false;
+			this.outlineColour = 0x2da4a8;
 		}
 		this.redraw();
 	};
@@ -34,17 +38,28 @@ THREE.KitchenWalls = function(){
 	};
 
 	this.redraw = function(){
-		this.kitchenWall = new THREE.Object3D();
+		var length = this.kitchenWall.children.length;
+		for(var i = 0; i < length; i++){
+			this.kitchenWall.remove(this.kitchenWall.children[i]);
+		}
 		if(this.layingOut){
-			drawWallOutline();
+			var mouseXY = getMouseXY();
+			circle.position.set(mouseXY.x, 1, mouseXY.z);
+			circle.visible = true;
+			this.drawWallOutline(mouseXY.x, mouseXY.z);
 		}
 		if(this.extruding){
-			drawWallOutline();
-			drawFootprint();
+			this.drawWallOutline();
+			this.drawFootprint();
 			if(this.height > 0){
 				drawWall();
 			}
-
+		}
+		if(this.editMode){
+			drawWall();
+		}
+		if(this.complete){
+			drawWall();
 		}
 	};
 
@@ -59,12 +74,31 @@ THREE.KitchenWalls = function(){
 		this.redraw();
 	};
 
-	function drawWallOutline(){
-
+	this.drawWallOutline = function (x, y){
+		for (var i = 0; i < this.plan.length; i++) {
+			if(i == this.plan.length - 1 && this.layingOut){
+				var wallEdge = cylinderAsLine(x, y, this.plan[i].x, this.plan[i].y, this.outlineColour);
+				this.kitchenWall.add(wallEdge);
+			}else{
+				if(i + 1 < this.plan.length){
+					var wallEdge = cylinderAsLine(this.plan[i].x, this.plan[i].y, this.plan[i + 1].x, this.plan[i + 1].y, this.outlineColour);
+					this.kitchenWall.add(wallEdge);
+				}else{
+					var wallEdge = cylinderAsLine(this.plan[i].x, this.plan[i].y, this.plan[0].x, this.plan[0].y, this.outlineColour);
+					this.kitchenWall.add(wallEdge);
+				}
+			}
+		};
 	}
 
-	function drawFootprint(){
+	this.drawFootprint = function(){
 
+		var extrudeShape = new THREE.Shape(this.plan);
+		var shapeGeom = new THREE.ShapeGeometry(extrudeShape);
+		var footprint = new THREE.Mesh(shapeGeom, darkOrange);
+		footprint.rotation.x = Math.PI/2;
+		footprint.position.y = 1;
+		this.kitchenWall.add(footprint);
 	}
 
 	function drawWall(){
@@ -79,5 +113,17 @@ THREE.KitchenWalls = function(){
 		extrusion.rotation.x = Math.PI/2;
 		extrusion.translateZ(-this.height);
 		this.kitchenWall.add(extrusion);
+	}
+
+	function getMouseXY(){
+		setAxisPosition(new THREE.Vector3(0,0,0));
+		var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+		var projector = new THREE.Projector();
+		projector.unprojectVector( vector, camera );
+		
+		var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+		var intersectPlane = raycaster.intersectObject(XZplane);
+		var nearest = roundToNearest(intersectPlane[0].point.x, intersectPlane[0].point.z, 100, 10);
+		return nearest;
 	}
 }
